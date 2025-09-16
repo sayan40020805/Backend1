04ec0c066e1f55b404391e09748c375810f19028
Sayan Majhi <161747341+sayan40020805@users.noreply.github.com>
2025-09-16 09:15:00 +0000

    ok

--- routes/polls.js
+++ routes/polls.js
@@ -77,4 +77,21 @@ router.post('/:id/vote', auth, async (req, res) => {
   }
 });
 
+// Delete poll
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const poll = await Poll.findById(req.params.id);
+    if (!poll) {
+      return res.status(404).json({ message: 'Poll not found' });
+    }
+    if (poll.creator.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ message: 'Not authorized to delete this poll' });
+    }
+    await Poll.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Poll deleted' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 module.exports = router;
